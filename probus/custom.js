22f73b4094de82e6309ad4a4996b97ddb52e6976
igor.veko <igby.veko@gmail.com>
2017-10-31 21:06:37 +0100

    Add files via upload

---
+++ probus/custom.js
@@ -0,0 +1,6 @@
+document.getElementById("logo").addEventListener("mouseover", function() {
+  document.getElementById("background").style.backgroundImage = "url(entry-background-sa-bojama.jpg)";
+}, false);
+document.getElementById("logo").addEventListener("mouseout", function() {
+   document.getElementById("background").style.backgroundImage = "url(entry-page-background.jpg)";
+}, false);
